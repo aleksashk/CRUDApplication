bbaf96215fc272ee8eaa72cbe99d46906e0168c6
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-18 18:45:42 +0300

    CRUD application add HTML form from Thymeleaf

--- src/main/java/ru/philimonov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/philimonov/springcourse/controllers/PeopleController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.philimonov.dao.PersonDAO;
+import ru.philimonov.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +34,19 @@ public class PeopleController {
         //Получим одного человека по id из DAO и передадим на отображение в представление
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
