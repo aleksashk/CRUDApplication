5ad4ebc739209ed89efd9eea0cca122cd9ab659d
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-19 13:58:31 +0300

    JDBC start

--- src/main/java/ru/philimonov/dao/PersonDAO.java
+++ src/main/java/ru/philimonov/dao/PersonDAO.java
@@ -3,6 +3,12 @@ package ru.philimonov.dao;
 import org.springframework.stereotype.Component;
 import ru.philimonov.models.Person;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,39 +18,72 @@ import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
     private static int PEOPLE_COUNTER;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNTER, "Frank", 54, "frank@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNTER, "Helen", 20, "helen@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNTER, "Richard", 24, "rich@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNTER, "Toby", 12, "ttt78@mail.ru"));
-    }
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+        private static final String USERNAME = "postgres";
+        private static final String PASSWORD = "password";
+
+        private static Connection connection;
+
+        static {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     public List<Person> index() {
+            List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String sgl = "select * from Person";
+            ResultSet resultSet = statement.executeQuery(sgl);
+
+            while (resultSet.next()){
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
-    public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-    }
+//    public Person show(int id) {
+//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+//    }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNTER);
-        people.add(person);
+        try {
+            Statement statement = connection.createStatement();
+            String sgl = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," + person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(sgl);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
+//    public void update(int id, Person updatePerson) {
+//        Person personToBeUpdated = show(id);
+//
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setAge(updatePerson.getAge());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
+//    }
 
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
-    }
-
-    public void delete(int id) {
-        people.removeIf(person -> person.getId() == id);
-    }
+//    public void delete(int id) {
+//        people.removeIf(person -> person.getId() == id);
+//    }
 }
--- src/main/java/ru/philimonov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/philimonov/springcourse/controllers/PeopleController.java
@@ -34,12 +34,12 @@ public class PeopleController {
         return "people/index";
     }
 
-    @GetMapping("/{id}")
-    public String show(@PathVariable(value = "id") int id, Model model) {
-        //Получим одного человека по id из DAO и передадим на отображение в представление
-        model.addAttribute("person", personDAO.show(id));
-        return "people/show";
-    }
+//    @GetMapping("/{id}")
+//    public String show(@PathVariable(value = "id") int id, Model model) {
+//        //Получим одного человека по id из DAO и передадим на отображение в представление
+//        model.addAttribute("person", personDAO.show(id));
+//        return "people/show";
+//    }
 
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") Person person) {
@@ -54,26 +54,26 @@ public class PeopleController {
         personDAO.save(person);
         return "redirect:/people";
     }
+//
+//    @GetMapping("/{id}/{edit}")
+//    public String edit(Model model, @PathVariable("id") int id) {
+//        model.addAttribute("person", personDAO.show(id));
+//        return "people/edit";
+//    }
 
-    @GetMapping("/{id}/{edit}")
-    public String edit(Model model, @PathVariable("id") int id) {
-        model.addAttribute("person", personDAO.show(id));
-        return "people/edit";
-    }
-
-    @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
-        if (bindingResult.hasErrors()) {
-            return "people/edit";
-        }
-        personDAO.update(id, person);
-        return "redirect:/people";
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id) {
-        personDAO.delete(id);
-        return "redirect:/people";
-    }
+//    @PatchMapping("/{id}")
+//    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult, @PathVariable("id") int id) {
+//        if (bindingResult.hasErrors()) {
+//            return "people/edit";
+//        }
+//        personDAO.update(id, person);
+//        return "redirect:/people";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String delete(@PathVariable("id") int id) {
+//        personDAO.delete(id);
+//        return "redirect:/people";
+//    }
 
 }
