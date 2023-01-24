4c312e05863b804658fcdfa8a74aa6b004c809c4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-01-24 18:38:18 +0300

    JPA

---
+++ src/main/java/ru/philimonov/springcourse/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package ru.philimonov.springcourse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.philimonov.models.Person;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
